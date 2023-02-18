3323b067a58cdca7602964cca27adf29d69b8ddb
vvn1000 <vortexninja2025@gmail.com>
2023-02-17 22:47:01 -0600

    Login feature. Not fully working yet, but in progress.

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! hello</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +20,82 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
+/*
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  AsyncStorage,
+  Touchable,
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+export default class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
+
+  componentDidMount() {
+    this._loadInitialState().done();
+  }
+
+  _loadInitialState = async () => {
+    
+    var value = await AsyncStorage.getItem('user');
+    if (value !== null) {
+      this.props.navigation.navigate('Profile');
+    }
+
+  }
+
+  render() {
+    return (
+      <KeyboardAvoidingView behavior='padding' style={styles.wrapper}>
+
+        <View style={styles.container}>
+
+          <Text style={styles.header}>- LOGIN -</Text>
+          
+          <TextInput
+              style={styles.textInput} placeholder='Username'
+              onChangeText={ (username) => this.setState({username}) }
+              underlineColorAndroid='transarent'
+          />
+
+          <TextInput
+              style={styles.textInput} placeholder='Password'
+              onChangeText={ (password) => this.setState({password}) }
+              underlineColorAndroid='transarent'
+          />
+        
+          <TouchableOpacity
+              style={styles.btn}
+              onPress={this.login}>
+                <Text>Log in</Text>
+          </TouchableOpacity>
+
+        </View>
+      
+      </KeyboardAvoidingView>
+
+    );
+  }
+
+  login = () => {
+      alert('test'); 
+  }
+}
+
+*/
