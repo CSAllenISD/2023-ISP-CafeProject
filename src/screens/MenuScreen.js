c3faa5cd651ff16f9b4f395f1323ea3e4682e765
saip9 <srinagasaisiv.paladugu@student.allenisd.org>
2023-03-26 12:26:40 -0500

    Added Add to Cart Button in MenuScreen.js Alert is shown when user clicks on the button

--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import  {Text, View, StyleSheet, Image} from 'react-native';
+import  {Text, View, StyleSheet, Image, Alert} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts, Mynerve_400Regular } from '@expo-google-fonts/mynerve';
+import CustomInput from '../components/CustomInput';
+import CustomButton from '../components/CustomButton/CustomButton';
+const addToCart =() => {
+  Alert.alert('Item Added Sucessfully', '', [
+
+    {text: 'OK'},
+  ]);
+}
 const Menu = () => {
   let [fontsLoaded] = useFonts({
     Mynerve_400Regular,
@@ -20,6 +28,8 @@ const Menu = () => {
           source={require('../../assets/images/chicken_burger.jpg')}
           />
               <Text style = {styles.description}> Chicken Sandwich</Text>
+              <Text style = {styles.price}> Price: $2.75</Text>
+             <CustomButton text="Add to Cart" onPress={addToCart} />
       </View>
   );
 };
@@ -66,10 +76,20 @@ const styles = StyleSheet.create({
       fontStyle: "italic",
       fontWeight: "500",
       fontFamily: "Mynerve_400Regular",
-      fontSize: 20,
+      fontSize: 25,
       textAlign: "center",
       padding: 20,
-      }
+      },
+
+      price: {
+        color: "black",
+        fontStyle: "italic",
+        fontWeight: "500",
+        fontFamily: "Mynerve_400Regular",
+        fontSize: 20,
+        textAlign: "center",
+        padding: 10,
+        }
 
 });
 
